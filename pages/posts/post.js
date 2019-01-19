10e04afa2ebe437f66a4646d4ae719642847386b
huanghanzhilian <1319639755@qq.com>
2019-01-20 00:13:05 +0800

    第五章

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -12,7 +12,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // this.data.postList =postsData.postList;
+    // this.data.posts_key = postsData.postList;
     this.setData({
       posts_key:postsData.postList
     });
--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -4,20 +4,5 @@ Page({
     wx.navigateTo({
       url:"../posts/post"
     });
-    //重定向 由当前路由平行跳转
-    // wx.redirectTo({
-    //   url:"../posts/post"
-    // })
-  },
-  onteztop:function(){
-    console.log(" ssssonteztop")
-  },
-  onHide:function(){
-    // 当页面隐藏
-     console.log("onHide")
-  },
-  onUnload:function(){
-    //当页面关闭
-     console.log("onUnload")
   }
 })
